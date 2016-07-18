305fee496976596bca513241c8f51f99809946dd
Adriano <adriano.franci>
2016-07-18 09:45:59 +0200

    Initial commit

--- app/main.js
+++ app/main.js
@@ -3,7 +3,7 @@ var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
 var app_component_1 = require("./component/AppComponent/app.component");
 var http_1 = require("@angular/http");
 var app_route_1 = require('./routes/app.route');
-require('angular2-materialize');
+require("angular2-materialize");
 //noinspection TypeScriptValidateTypes
 platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent, [
     http_1.HTTP_PROVIDERS,
--- app/main.ts
+++ app/main.ts
@@ -5,8 +5,8 @@ import {AppComponent} from "./component/AppComponent/app.component";
 
 import {HTTP_PROVIDERS} from "@angular/http";
 import { AppRouteProvider } from './routes/app.route';
-import 'angular2-materialize';
-import {provideForms, disableDeprecatedForms} from "@angular/forms";
+import "angular2-materialize";
+
 
 //noinspection TypeScriptValidateTypes
 bootstrap(AppComponent,[
